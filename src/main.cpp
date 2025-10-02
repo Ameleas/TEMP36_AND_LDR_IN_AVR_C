1f77542d84e0770975ab8787fca9467761082dc3
Amelea <amelea.nordfors@yh.nackademin.se>
2025-10-02 12:24:17 +0200

    cleaned main

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,22 @@
-/*#include <avr/io.h>
-#include "avr_adc.h"
-#include "avr_usart.h"
-#include <util/delay.h>*/
+#include <util/delay.h>
 #include "sensors.h"
 
 
 int main(void)
 {
-	/* ADC Configuration and Init 
-	ADC_ConfigData ADC_Data;
-	ADC_Data.Prescaler = ADC_PRESCALER_128;
-	ADC_Data.VoltageReference = ADC_VOLTAGE_REFERENCE_AVCC;
-	ADC_Init(ADC_Data);*/
-	
-	/* USART Configuration and Init 
-	USART_ConfigData USART_Data;
-	USART_Data.BaudRate = 9600;
-	USART_Data.DataBit = USART_DATA_BIT_EIGHT;
-	USART_Data.ParityBit = USART_PARITY_BIT_NO;
-	USART_Data.StopBit = USART_STOP_BIT_ONE;
-	USART_Data.UsartMode = USART_MODE_ASYNCHRONOUS;
-	USART_Init(USART_Data);*/
-    
-	/* Local Variables 
-	uint16_t ADC_RawData = 0x0000;
-	float TMP36_Temp = 0.0f;
-  float voltage = 0.0f;*/
-	
     
   initAdc();
   initUsart();
 
     while (1) 
     {
-		/* Read ADC Value at Pin PC0 (A0/ADC0)
-		ADC_RawData = ADC_ReadPin(ADC0);
-		
-		// Convert the raw ADC value to voltage 
-		voltage = ((float)( ADC_RawData * 5 ) / 1023);
-		
-		// Convert to Degrees Celsius 
-		TMP36_Temp = (voltage - 0.5) * 100.0;
-		
-		// Print the data in terminal 
-		USART_TransmitString("\nTMP36 Temperature Degree C = ");
-		USART_WaitTillTransmitFree();
-		USART_TransmitFloat(TMP36_Temp,3);*/
-		
-		/* Wait for 500ms */
     readTmp36();
     readLDR();
-    printTmp36();
-    printLDR();
-    
-
-		_delay_ms(500);
+    printSensorValue();
+  
+    /* Wait for 10s */
+		_delay_ms(10000);
 	
     }
+    
 }
